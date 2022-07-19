5471f06bb962b3878dc074d522a7fc0c5ba0616f
arad4228 <arad4228@gmail.com>
2022-07-19 19:34:32 +0900

    Add my code printer

--- byte-test/main.cpp
+++ byte-test/main.cpp
@@ -1,6 +1,7 @@
 #include <stddef.h> // for size_t
 #include <stdint.h> // for uint8_t
 #include <stdio.h> // for printf
+#include <netinet/in.h>
 
 void dump(void* p, size_t n) {
 	uint8_t* u8 = static_cast<uint8_t*>(p);
@@ -41,15 +42,19 @@ uint32_t my_ntohl(uint32_t n)
 void  write_0x1234() {
 	uint8_t network_buffer[] = { 0x12, 0x34 };
 	uint16_t* p = reinterpret_cast<uint16_t*>(network_buffer);
-	uint16_t n = my_ntohs(*p); // TODO
+	uint16_t n = ntohs(*p); // TODO
+	uint16_t n1 = my_ntohs(*p);
 	printf("16 bit number=0x%x\n", n);
+	printf("My 16 bit number=0x%x\n", n1);
 }
 
 void  write_0x12345678() {
 	uint8_t network_buffer[] = { 0x12, 0x34, 0x56, 0x78 };
 	uint32_t* p = reinterpret_cast<uint32_t*>(network_buffer);
-	uint32_t n = my_ntohl(*p); // TODO
+	uint32_t n = ntohl(*p); // TODO
+	uint32_t n1 = my_ntohl(*p);
 	printf("32 bit number=0x%x\n", n);
+	printf("My 32 bit number=0x%x\n",n1);
 }
 
 int main() {
